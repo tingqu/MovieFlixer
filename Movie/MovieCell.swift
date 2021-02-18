a8232c718cc459a7eb29dcb360d7fd3ce2588cea
Sylvia Qu <quting3@gmail.com>
2021-02-17 22:23:25 -0500

    Uploading the files

---
+++ Movie/MovieCell.swift
@@ -0,0 +1,30 @@
+//
+//  MovieCell.swift
+//  Movie
+//
+//  Created by Ting Qu on 2/17/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
